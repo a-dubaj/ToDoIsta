911301afa808aa6a45fe98d84503308317a9a8e6
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-18 02:15:26 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use termion::{color, style, terminal_size};
-use std::io::{Write, stdout};
+use std::io::{Write, stdout, stdin};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn main() {
+    let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
-    print!("{}{}",termion::clear::All, termion::cursor::Goto(1,1) );
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            _ => {}
+        }
+    }
 }

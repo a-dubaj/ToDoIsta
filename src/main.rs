4f7054fad8751bab161a5cabc39d8b8868da22bc
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-23 19:29:14 +0200

    Udpate

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     while !quit {
         let key = getch();
         match key as u8 as char {
-           'q' => quit = true,
-            _=> {}
+            'q' => quit = true,
+            _ => {}
         }
     }
     endwin();
